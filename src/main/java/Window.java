37e0a9356a6df743ef6fe3777a33b76dbcf20285
filip_jonckers <filip.jonckers@gmail.com>
2020-11-15 22:38:24 +0100

    mac OSX platform OpenGL options

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -57,6 +57,7 @@ public class Window {
             glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
             glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
             glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
         }
 
         glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
